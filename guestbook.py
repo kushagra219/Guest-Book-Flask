3f06867f94bdcd51d66932adc03d8a3fb151fc65
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-03-18 12:03:01 +0530

    second

--- guestbook.py
+++ guestbook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,10 +10,12 @@ def index():
 def sign():
     return render_template('sign.html')
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    pass
-
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    
+    return render_template('index.html', name=name, comment=comment)
 
 if __name__ == '__main__':
     app.run(debug=True)
